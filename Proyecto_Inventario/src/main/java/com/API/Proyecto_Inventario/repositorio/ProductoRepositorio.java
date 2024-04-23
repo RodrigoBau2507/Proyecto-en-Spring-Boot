9e3439da6c9d5ffc219a36a2a79da81c1cd74f59
Rodrigo Bautista <91805080+RodrigoBau2507@users.noreply.github.com>
2024-04-23 02:29:56 -0600

    Delete Proyecto_Inventario directory

--- Proyecto_Inventario/src/main/java/com/API/Proyecto_Inventario/repositorio/ProductoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.API.Proyecto_Inventario.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.API.Proyecto_Inventario.model.Producto;
-
-public interface ProductoRepositorio extends CrudRepository<Producto, Integer> {
-
-}
